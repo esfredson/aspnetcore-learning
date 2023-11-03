5c292dc66642fba16b32e9e591b62599edb806f7
esfredson <federicofrasquet@gmail.com>
2023-11-03 20:29:21 +0100

    Agregar archivos de proyecto.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.HttpLogging;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts =>
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter(
+    "Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Fede", "Frasquet"));
+
+app.Run();
+
+public record Person(string Firstname, string Lastname);
